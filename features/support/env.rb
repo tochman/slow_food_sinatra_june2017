8e5dd35d291313f9395428ce04a5eb098b804a24
tochman <thomas@craftacademy.se>
2017-07-12 10:34:48 +0200

    initializes cucumber

---
+++ features/support/env.rb
@@ -0,0 +1,29 @@
+
+ENV['RACK_ENV'] = 'test'
+
+require './lib/controller.rb'
+
+require 'capybara'
+require 'capybara/cucumber'
+require 'rspec'
+require 'pry'
+require 'database_cleaner'
+require 'database_cleaner/cucumber'
+
+Capybara.app = SlowFood
+
+class SlowFoodWorld
+  include Capybara::DSL
+  include RSpec::Expectations
+  include RSpec::Matchers
+end
+
+DatabaseCleaner.strategy = :truncation
+
+Around do |_scenario, block|
+  DatabaseCleaner.cleaning(&block)
+end
+
+World do
+  SlowFood.new
+end
